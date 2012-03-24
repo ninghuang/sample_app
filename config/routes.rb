ee849084a2c9cb4df0fe630eec3ac9acb4173b3b
ninghuang <ninghuang123@yahoo.com>
2012-03-24 15:38:58 -0400

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+    match '/signup',  :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+
   root :to => 'pages#home'
    # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
@@ -58,7 +61,7 @@ SampleApp::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
 
-  # See how all your routes lay out with "rake routes"
+  # See how all your routes lay config/routes.rbout with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
